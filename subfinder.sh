d16000a99a693bef4e3cd73e65f68bca1a87ab23
hidog123 <52358194+hidog123@users.noreply.github.com>
2022-12-01 15:10:01 +0100

    Create subfinder.sh

---
+++ subfinder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash    
+parametre==$
+echo go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+echo subfinder -d $ > /home/kali/subfinder/subf/suby.txt
+echo python3  /home/kali/subf/subfinder.py
+cat  /home/kali/subf/resulte.txt
