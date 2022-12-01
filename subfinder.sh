75e41664ab5434070b51b81baf2769f6ef776e46
hidog123 <52358194+hidog123@users.noreply.github.com>
2022-12-01 15:12:03 +0100

    Update subfinder.sh

--- subfinder.sh
+++ subfinder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash    
 parametre==$
 echo go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-echo subfinder -d $ > /home/kali/subfinder/subf/suby.txt
+echo subfinder -d $ > /home/kali/subfinder/sub/suby.txt
 echo python3  /home/kali/subf/subfinder.py
 cat  /home/kali/subf/resulte.txt
